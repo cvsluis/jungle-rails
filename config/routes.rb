67a60e4eed6e6ff160ddda6948f9655e1ede273b
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-10-28 13:49:00 -0700

    fix: remove extra end

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,6 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
-end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
