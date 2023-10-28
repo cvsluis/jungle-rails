5b5f8628e3fab225f9286e9d76a82137474c6c2b
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-10-28 12:58:43 -0700

    feat: add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
     resources :categories, except: [:destroy, :edit, :update, :show]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
