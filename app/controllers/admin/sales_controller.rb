cf435a3cec243992ed63901f95c879f1ec5b242e
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-10-28 23:02:12 -0700

    feat: add new sale form

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -5,4 +5,28 @@ class Admin::SalesController < ApplicationController
     @sales = Sale.all
   end
 
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(product_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:sale).permit(
+      :name,
+      :percent_off,
+      :starts_on,
+      :ends_on
+    )
+  end
 end
