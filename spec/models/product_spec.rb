d57cd5ed595dee4b1d2e8cf5916f1fef1b013388
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-07 16:34:39 -0800

    feat: add initial product validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe ProductSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    before do
+      @category = Category.create(name: 'Flowers')
+      @product = @category.products.create({ name: 'Tulip', quantity: 10, price: 5.99 })
+    end
+
+    it "is valid with valid attributes" do
+      expect(@product).to be_valid
+    end
+
+
+    it "is not valid without a name"
+    it "is not valid without a price"
+    it "is not valid without a quantity"
+    it "is not valid without a categrory"
+  end
 end
