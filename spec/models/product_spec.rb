55f28abb2754dcc54da9866dd03660b7a91360fc
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-07 16:42:23 -0800

    feat: implement all product test validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,10 +12,29 @@ RSpec.describe Product, type: :model do
       expect(@product).to be_valid
     end
 
+    it "is not valid without a name" do
+      @product.name = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Name can't be blank"])
+    end
 
-    it "is not valid without a name"
-    it "is not valid without a price"
-    it "is not valid without a quantity"
-    it "is not valid without a categrory"
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+    end
+
+    it "is not valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Quantity can't be blank"])
+    end
+
+    it "is not valid without a categrory" do
+      @product.category = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Category must exist", "Category can't be blank"])
+    end
+
   end
 end
