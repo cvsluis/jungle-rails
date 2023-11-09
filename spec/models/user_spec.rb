93b39f233d375f19da9316e3168bfc63d2c42f02
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-08 16:52:22 -0800

    test: add user password and confirmation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    before do
+      @user = User.create(first_name: 'Elizabeth', last_name: 'Bennet', email: 'lizzy@bennet.com', password: 'darcyluver', password_confirmation: 'darcyluver')
+    end
+
+    it "must be created with a password and password_confirmation fields" do
+      @user.password = nil
+      expect(@user).not_to be_valid
+    end
+
+    it "is not valid when password and password_confirmation fields do not match" do
+      @user.password_confirmation = 'darcylover'
+      expect(@user).not_to be_valid
+    end
+  end
 end
