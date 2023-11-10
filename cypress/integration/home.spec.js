714a2c83dd59dee390a9032651809bd28af08c22
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-09 22:15:20 -0800

    test: check for number of test products on page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,4 +10,8 @@ describe('jungle-rails app', () => {
   it("displays products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("displays 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
